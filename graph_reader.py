3b50d90234920b67d1d370d4def78a4aca911c45
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-10-13 00:35:11 -0700

    Recovering lost commit history

--- graph_reader.py
+++ graph_reader.py
@@ -1,193 +1,133 @@
 import MyCamera as webcam
 import cv2
 import numpy as np
+import py_graph_cpp
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
-def detector():
-    params = cv2.SimpleBlobDetector_Params()
-
-    # Change thresholds
-    params.minThreshold = 0
-    params.maxThreshold = 256
-    # Filter by Area.
-    params.filterByArea = True
-    params.minArea = 70
-    params.maxArea = 1000
-    # Filter by Circularity
-    #params.filterByCircularity = True
-    params.minCircularity = .8
-    # Filter by Convexity
-    #params.filterByConvexity = True
-    params.minConvexity = 0.9
-    # Filter by Inertia
-    params.filterByInertia = True
-    params.minInertiaRatio = 0
-    # Filter by Color
-    params.filterByColor = True
-    params.blobColor = 255
-
-    detector = cv2.SimpleBlobDetector_create(params)
-
-    return detector
-
-
-root = tk.Tk()
-
-class Application(tk.Frame, threading.Thread):
-    def __init__(self, master=None):
-        super().__init__(master)
-
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.show_threshold = tk.Button(self)
-        self.show_threshold["text"] = "Threshold Image"
-        self.show_threshold["command"] = self.say_hi
-        self.show_threshold.pack(side="top")
-
-        self.show_color = tk.Button(self)
-        self.show_color["text"] = "Color Image"
-        self.show_color["command"] = self.say_hi
-        self.show_color.pack(side="top")
-
-        self.show_canny = tk.Button(self)
-        self.show_canny["text"] = "Canny Image"
-        self.show_canny["command"] = self.say_hi
-        self.show_canny.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-
-#app = Application(master=root)
-# app.start()
-
+# select the webcam the reader uses if the computer has multiple connected
 capture = webcam.MyCamera(1, "output 1")
-#bc = webcam.MyCamera(0, "output 0")
-#ret = capture.__setattr__('CV_CAP_PROP_FRAME_WIDTH', 320)
-#ret = capture.__setattr__('CV_CAP_PROP_FRAME_WIDTH', 280)
-#ret = bc.__setattr__('CV_CAP_PROP_FRAME_WIDTH', 320)
-#ret = bc.__setattr__('CV_CAP_PROP_FRAME_WIDTH', 280)
 
-box_corners = np.array([(380, 50), (680, 50), (680, 400), (380, 400)])
-#box = cv2.boundingRect(box_corners)
-
-frame1_copy = capture.frame1
-display_images = False
-detector = detector()
-
-
+# initialize video recording
+out = cv2.VideoWriter("output_video.avi", fourcc, 20.0, (640, 480))
 
 while True:
+    py_graph_cpp.resetGraph()
 
-    frame1_copy = t_frame1_copy.copy()
-    grey_blurred = t_grey_blurred.copy()
-    img_canny = t_img_canny.copy()
-    gray_threshold = t_gray_threshold.copy()
-    keypoints = detector.detect(gray_threshold)
-    test_mask = np.ones(gray_threshold.shape, np.uint8)
-    # Draw detected blobs as red circles.
-    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-    #im_with_keypoints = cv2.drawKeypoints(test_mask, keypoints, np.array([]), (255, 255, 255),
-    #                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # update frames
+    capture.tick()
+    frame1_copy = capture.getFrame("color").copy()
+    img_grey = capture.getFrame("grey_blurred").copy()
+    img_canny = capture.getFrame("canny").copy()
 
-    cv2.erode(img_canny, (3, 3), img_canny)
-    cv2.dilate(img_canny, (5, 5), img_canny)
-
-
-
+    # process canny image and find contours
+    cv2.dilate(img_canny, np.ones((3, 3)), img_canny, iterations=2)
+    cv2.erode(img_canny, np.ones((2, 2)), img_canny)
     _, contours, hierarchy = cv2.findContours(img_canny.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(len(contours))
-    # Create a black image
 
-    # Write some Text
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    centers = np.zeros((len(contours),2))
-    biggest = (0, 0, 0, 0)
+    # skip iteration if no contours are found
+    if len(contours) == 0:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        continue
+
+    # find the centers of all potential points in list of contours
+    # assume largest contour is the paper
+    centers = []
+    biggest_contour_area = 0
+    biggest_contour = contours[0]
+    biggest_index = 0
     for i in range(len(contours)):
-        cnt = contours[i]
-        bx,by,bw,bh = cv2.boundingRect(cnt)
-
-        if bh + bx > biggest[1] + biggest[3]:
+        contour = contours[i]
+        bx, by, bw, bh = cv2.boundingRect(contour)
+        contour_area = cv2.contourArea(contour)
+        if 150000 > contour_area > biggest_contour_area:
+            biggest_contour = contour
+            biggest_contour_area = cv2.contourArea(contour)
             biggest = (bx, by, bw, bh)
+            biggest_index = i
+        centers.append((int((bx + bx + bw) / 2), int((by + by + bh) / 2)))
 
-        centers[i][0] = int((bx + bx + bw) / 2)
-        centers[i][1] = int((by + by + bh) / 2)
+    # find and display corner points of paper
+    epsilon = 0.1 * cv2.arcLength(biggest_contour, True)
+    approx = cv2.approxPolyDP(biggest_contour, epsilon, True)
+    cv2.drawContours(frame1_copy, approx, -1, (0, 255, 255), thickness=15)
 
+    # display coordinates in the form (x,y)
+    if len(approx) == 4:
+        b1 = approx[0][0]
+        b2 = approx[1][0]
+        b3 = approx[2][0]
+        b4 = approx[3][0]
+        b1_str = "(" + str(b1[0]) + "," + str(b1[1]) + ")"
+        b2_str = "(" + str(b2[0]) + "," + str(b2[1]) + ")"
+        b3_str = "(" + str(b3[0]) + "," + str(b3[1]) + ")"
+        b4_str = "(" + str(b4[0]) + "," + str(b4[1]) + ")"
+        cv2.putText(frame1_copy, b1_str, (b1[0] - 5, b1[1] + 5),
+                    font, .6, (0, 0, 255), 2)
+        cv2.putText(frame1_copy, b2_str, (b2[0] - 5, b2[1] + 5),
+                    font, .6, (0, 0, 255), 2)
+        cv2.putText(frame1_copy, b3_str, (b3[0] - 5, b3[1] + 5),
+                    font, .6, (0, 0, 255), 2)
+        cv2.putText(frame1_copy, b3_str, (b4[0] - 5, b4[1] + 5),
+                    font, .6, (0, 0, 255), 2)
+
+    # iterate through all potential points and find which ones are within the paper contour
+    graph_points = []
+    vertex_id = 0
     for i in range(len(contours)):
-        output = str(i)
+        if len(approx) != 4:
+            break
         x = int(centers[i][0])
         y = int(centers[i][1])
-        bx = biggest[0]
-        by = biggest[1]
-        bw = biggest[2]
-        bh = biggest[3]
-        print(biggest)
-        if x < bx or x > (bw + bx) or y < by or y >= (by + bh):
-            continue
-        if biggest != (bx , by, bw, bh):
-            cv2.putText(frame1_copy, output, (x, y), font, .6, (0, 255, 00), 1)
+        in_square = cv2.pointPolygonTest(approx, (x, y), True)
+        # draw vertex if it is inside the paper contour
+        # add vertex to graph_point list for TSP calculation
+        if in_square > 20 and 5 < cv2.contourArea(contours[i]) < 300:
+            print("Vertex " + str(vertex_id) + str((x, y)) +
+                  " distance from edge of paper: " + str(in_square))
+            # cv2.putText(frame1_copy, str(vertex_id), (x + 5, y + 5), font, .6, (0, 0, 255), 2)
+            graph_points.append((x, y))
             cv2.circle(frame1_copy, (x, y), 7, (0, 0, 255), -1)
-        else:
-            cv2.circle(frame1_copy, (x, y), 7, (0, 255, 0), -1)
+            py_graph_cpp.addVertex(vertex_id, x, y)
+            vertex_id += 1
 
-    #cv2.floodFill(im_with_keypoints, None, (0,0), (255,255,255))
-    #cv2.dilate(im_with_keypoints, (3,3), im_with_keypoints)
-    #cv2.imshow("vertices", im_with_keypoints)
+    # skip TSP calculation if there are too many points in order to prevent lag
+    if len(graph_points) > 1000:
+        continue
 
+    # compute the TSP and draw lines
+    tsp_path = py_graph_cpp.VectorInt(py_graph_cpp.getTwoOptRoute())
+    next_x = 0
+    next_y = 0
+    for i in range(len(tsp_path) - 1):
+        current_id = tsp_path[i]
+        next_id = tsp_path[i+1]
+        current_x = graph_points[current_id][0]
+        current_y = graph_points[current_id][1]
+        next_x = graph_points[next_id][0]
+        next_y = graph_points[next_id][1]
+        cv2.line(frame1_copy,
+                 (current_x, current_y),
+                 (next_x, next_y),
+                 (255, 0, 0), thickness=3)
+    # draw line connecting the end back to the beginning
+    if len(tsp_path) > 1:
+        next_id = tsp_path[0]
+        cv2.line(frame1_copy, (next_x, next_y),
+                 (graph_points[next_id][0], graph_points[next_id][1]),
+                 (255, 0, 0), thickness=3)
 
-    #bc.tick()
-    #wc_frame1 = bc.getFrame("color")
-    #bc_frame1 = bc.getFrame("color")
-   # bc_gray = bc.getFrame("grey_blurred")
-    #bc_canny = bc.getFrame("canny")
-
-    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-    #cv2.resize(grey_blurred, (3, 3), grey_blurred, 320, 280)
-    #cv2.resize(bc_gray, (3, 3), bc_gray, 320, 280)
-
-
-    #cv2.imshow("bad camera color", bc_frame1)
-
-    #cv2.imshow("web camera color", wc_frame1)
-
-
-    imgInverted = capture.getFrame("inverted", img_canny.copy())
-    imgThresh = capture.getFrame("threshold")
-    #imgThreshInverted = capture.getFrame("inverted", imgThresh.copy())
-    #cv2.erode(imgInverted, (9, 9), imgInverted)
-
-    #cv2.rectangle(frame1_copy, (50, 50), (400, 300), (255, 255, 255), 3)
-    #contours, heirarchy] = cv2.findContours(imgInverted.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    #cv2.drawContours(imgThresh, contours[0], -1, (255,255,255))
-
+    # write and display frame
+    out.write(frame1_copy)
     cv2.imshow("window", frame1_copy)
-    #cv2.imshow("inverted", imgInverted)
-    #cv2.imshow("threshold", imgThresh)
-    #cv2.imshow("canny", img_canny)
+    cv2.imshow("canny", img_canny)
 
-
-
-    cv2.imshow("threshold", gray_threshold)
+    # end program if q key is press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.imwrite("last_frame.jpeg", frame1_copy)
         break
-    if cv2.waitKey(1) & 0xFF == ord('g'):
-        display_images = True
-    if cv2.waitKey(1) & 0xFF == ord('x'):
-        cv2.destroyWindow("gray")
-        cv2.destroyWindow("canny")
-        display_images = False
-        #plt.show()
-
-    if display_images is True:
-        cv2.imshow("gray", grey_blurred)
-        cv2.imshow("canny", img_canny)
 
 # Release everything if job is finished
 out.release()
